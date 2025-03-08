bc1067ab6bedfcdf94012cbcb3d01b365d225cbf
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:38:17 +0530

    Add files via upload

---
+++ PrintNumbersWhileLoop.java
@@ -0,0 +1,10 @@
+package Loops;
+public class PrintNumbersWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 20) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
